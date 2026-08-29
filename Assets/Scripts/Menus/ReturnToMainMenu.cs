86d886fb02b449607c2bc05fd82f41120d66c5f4
realGuybrush <TripleBaka@yandex.ru>
2026-08-29 22:13:17 +0200

    Changed tons of animations of cutscenes.

--- Assets/Scripts/Menus/ReturnToMainMenu.cs
+++ Assets/Scripts/Menus/ReturnToMainMenu.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ReturnToMainMenu : MonoBehaviour
-{
+{   
+    public void Restart()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    
     public void Exit()
     {
         SceneManager.LoadScene("MainMenuScene");
